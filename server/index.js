1cd6448a6e0a1c36e325aeef14ad57d5acf5ffa3
Harshit Rautela <harshitsrautela@gmail.com>
2024-08-14 16:12:22 +0530

    yes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://blogging-aahn.vercel.app' // Allow requests from your frontend domain
+}));
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
