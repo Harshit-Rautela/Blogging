b616a92f88bceffb6500f1135ea0d70ef128c23a
Harshit Rautela <harshitsrautela@gmail.com>
2024-08-14 10:12:19 +0530

    msg

--- server/index.js
+++ server/index.js
@@ -12,13 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://blogging-aahn.vercel.app",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.get("/", (request, response) => {
   console.log(request);
