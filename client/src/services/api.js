078ab29a4af81def01f12019eab3b7eff23d8f1a
Harshit Rautela <harshitsrautela@gmail.com>
2024-08-14 14:21:04 +0530

    msd

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
 
-// Create an Axios instance with default configurations
-const api = axios.create({
-  baseURL: 'https://blogging-ten-nu.vercel.app', // Your backend base URL
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
 
 // Function to handle user signup
 export const signupUser = async (userData) => {
   try {
-    const response = await api.post('/auth/register', userData);
+    const response = await axios.post('https://blogging-ten-nu.vercel.app/auth/register', userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     return response.data; // Returns the response data, which might include a token
   } catch (error) {
     throw error; // Rethrow the error to handle it in the component
@@ -20,7 +17,11 @@ export const signupUser = async (userData) => {
 
 export const loginUser = async (userData) => {
     try {
-      const response = await api.post('/auth/login', userData);
+      const response = await axios.post('https://blogging-ten-nu.vercel.app/auth/login', userData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
       return response.data; // Returns the response data, which might include a token
     } catch (error) {
       throw error; // Rethrow the error to handle it in the component
