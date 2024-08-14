c7a62c86633132aa86fbf3b95ecb1d9797f2d922
Harshit Rautela <harshitsrautela@gmail.com>
2024-08-14 15:58:58 +0530

    ch

--- server/index.js
+++ server/index.js
@@ -1,40 +1,45 @@
-import express from "express";
+import express from 'express';
+import path from 'path';
+import { MongoDBURL } from './config.js';
+import Blogrouter from './routes/Routes.js';
+import Userrouter from './routes/auth.js';
+import mongoose from 'mongoose';
+import { User } from './models/Model.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import { MongoDBURL } from "./config.js";
-import Blogrouter from "./routes/Routes.js";
-import Userrouter from "./routes/auth.js";
-import mongoose from "mongoose";
-import { User } from "./models/Model.js";
-import cors from "cors";
-import env from "dotenv/config.js";
+// Initialize environment variables
+dotenv.config();
+
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 // Serve static files from the 'public' directory
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (request, response) => {
-  console.log('the request is : ',request);
-  return response.status(200).send("Welcome To Blogging");
+// Routes
+app.get("/", (req, res) => {
+  console.log('The request is:', req);
+  return res.status(200).send("Welcome To Blogging");
 });
-app.use("/auth", [Userrouter]);
-app.use("/", [Blogrouter]);
 
-mongoose
-  .connect(MongoDBURL)
+app.use("/auth", Userrouter);
+app.use("/", Blogrouter);
+
+// Connect to MongoDB
+mongoose.connect(MongoDBURL)
   .then(() => {
     console.log("App connected to MongoDB database");
   })
   .catch((error) => {
-    console.log(error);
+    console.error("Error connecting to MongoDB:", error);
   });
 
+// Start the server
 app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
+  console.log(`App is listening on port ${PORT}`);
 });
