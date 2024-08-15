8dce34d40b142c23f372330a5c39a6911ed8c90a
Harshit Rautela <harshitsrautela@gmail.com>
2024-08-15 12:18:18 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -18,12 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true // Allow sending cookies
-}));
+app.use(cors());
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
