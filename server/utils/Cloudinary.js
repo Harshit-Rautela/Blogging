64bcd0b13669c43aa34515607b7cd430b868d642
Harshit <harshitsrautela@gmail.com>
2025-12-01 14:52:42 +0530

    image upload added

--- server/utils/Cloudinary.js
+++ server/utils/Cloudinary.js
@@ -1,39 +1,27 @@
-import { v2 as cloudinary } from 'cloudinary';
-import fs from 'fs';
-import {config } from 'dotenv';
-
+import { v2 as cloudinary } from "cloudinary";
+import { config } from "dotenv";
 config();
- // Configuration
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
- 
-const uploadOnCloudinary = async(localFilePath)=>{
-    try {
-        if(!localFilePath) return null;
-        //Upload the file on cloudinary
-        const normalizedFilePath = localFilePath.replace(/\\/g, '/');
-        if (!fs.existsSync(normalizedFilePath)) {
-            console.error('File does not exist at path:', normalizedFilePath);
-            return null;
-        }else{
-            console.log('It exists')
-        }
-         
-       const uploadResult = await cloudinary.uploader.upload(normalizedFilePath,{
-            //can put any resource type, here auto means it will detect itself whether its image or pdf etc.
-            resource_type:"auto"
-        })
-        //now file is uploaded successfully
-        console.log("File is uploaded on cloudinary",uploadResult.url)
-        return uploadResult;
-    } catch (err) {
-        //fs.unlinkSync(localFilePath) //remove the locally saved temporary file
-        console.error('Error uploading file to Cloudinary:', err.message);
-           return null;
-        }
-}
 
-export default uploadOnCloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (fileBuffer) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto" },
+      (error, result) => {
+        if (error) {
+          console.error("Cloudinary upload error:", error);
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+
+    uploadStream.end(fileBuffer);
+  });
+};
