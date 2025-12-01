925447069d045c523629cc6bfc4038d011a86698
Harshit <harshitsrautela@gmail.com>
2025-12-01 15:31:32 +0530

    json

--- client/src/services/BlogsApi.js
+++ client/src/services/BlogsApi.js
@@ -12,7 +12,7 @@ export const createBlog = async (blogData, token) => {
     const response = await axios.post(`${BACKEND_BASE_URL}`, blogData, {
       headers: {
         'x-auth-token': token,
-         'Content-Type': 'application/json'
+        //  'Content-Type': 'application/json'
         //  'Content-Type': 'multipart/form-data' // Uncomment if you are sending files
       }
     });
